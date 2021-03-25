d36d1ea87158bed4837d6601865cec266463d2c4
Padmajeet Pawar <54483358+padmajeetpawar@users.noreply.github.com>
2021-03-25 14:55:20 -0500

    Update FloorOfANumber.java

--- FloorOfANumber.java
+++ FloorOfANumber.java
@@ -26,6 +26,5 @@ class FloorOfANumber {
         System.out.println(FloorOfANumber.searchFloorOfANumber(new int[] { 4, 6, 10 }, 6));
         System.out.println(FloorOfANumber.searchFloorOfANumber(new int[] { 1, 3, 8, 10, 15 }, 12));
         System.out.println(FloorOfANumber.searchFloorOfANumber(new int[] { 4, 6, 10 }, 17));
-        System.out.println(FloorOfANumber.searchFloorOfANumber(new int[] { 4, 6, 10 }, -1));
     }
 }
